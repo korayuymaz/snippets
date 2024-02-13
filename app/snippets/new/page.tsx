b0a4b662c5d961f1e6d48a9ea5ef0407c83bd208
Koray <koray.uymaz@agesa.com.tr>
2024-02-13 19:52:01 +0300

    Adding a snippet to db with server action

--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,3 +1,55 @@
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
 export default function SnippetCreatePage() {
-  return <div>Create a Snippet!</div>;
+  async function createSnippet(formData: FormData) {
+    // This needs to be a server action!
+    "use server";
+
+    // Check the user's inputs and make sure they are valid
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    // Create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log(snippet);
+    // Redirect the user backt to the root route
+    redirect("/");
+  }
+
+  return (
+    <form action={createSnippet}>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flext flex-col gap-4">
+        <div className="flex gap-4">
+          <label htmlFor="title" className="w-12">
+            Title
+          </label>
+          <input
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label htmlFor="code" className="w-12">
+            Code
+          </label>
+          <textarea
+            name="code"
+            className="border rounded p-2 w-full"
+            id="code"
+          />
+        </div>
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
 }
