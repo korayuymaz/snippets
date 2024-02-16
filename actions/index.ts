cbc3729726a735e4831d8f2455bd88bd3f669894
Koray <koray.uymaz@agesa.com.tr>
2024-02-16 19:30:02 +0300

    Error added

--- actions/index.ts
+++ actions/index.ts
@@ -19,3 +19,46 @@ export async function deleteSnippet(id: number) {
 
   redirect("/");
 }
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  try {
+    // Check the user's inputs and make sure they are valid
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
+
+    if (typeof code !== "string" || code.length < 3) {
+      return {
+        message: "Code must be longer",
+      };
+    }
+    // Create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        message: error.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong...",
+      };
+    }
+  }
+
+  // Redirect the user backt to the root route
+  redirect("/");
+}
